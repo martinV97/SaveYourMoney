45d9c06fce81e6d4ae7881075604d2777cf9c3eb
martinV97 <matin.vn11@gmail.com>
2017-08-17 14:04:24 -0500

    Update

--- SaveYourMoney/app.js
+++ SaveYourMoney/app.js
@@ -39,3 +39,42 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.get('/expenses', function(req, res, next) {
+	var results = {};
+	results.Expenses = [];
+	var data = [];
+		var query = client.query('SELECT * FROM public.expenses');
+	    query.on('row', function (row){
+	      results.Expenses.push(row);
+	    });
+	    query.on('end', function (){
+	      return res.json(results);
+	    });
+});
+
+app.get('/profile', function(req, res, next) {
+	var results = {};
+	results.Profiles = [];
+	var data = [];
+		var query = client.query('SELECT * FROM PROFILE');
+	    query.on('row', function (row){
+	      results.Profiles.push(row);
+	    });
+	    query.on('end', function (){
+	      return res.json(results);
+	    });
+});
+
+app.get('/expensesProfile', function(req, res, next) {
+	var results = {};
+	results.ExpensesProfile = [];
+	var data = [];
+		var query = client.query('SELECT * FROM EXPENSES_PROFILE');
+	    query.on('row', function (row){
+	      results.ExpensesProfile.push(row);
+	    });
+	    query.on('end', function (){
+	      return res.json(results);
+	    });
+});
